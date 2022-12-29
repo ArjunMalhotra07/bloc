7c6a223b53dd64e256f9f5da29c784f47b3e29f4
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-12-29 10:14:27 +0530

    Bloc to Bloc Communication

---
+++ lib/logic/cubit/internet_cubit.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test/constants/enum.dart';
+
+part 'internet_state.dart';
+
+class InternetCubit extends Cubit<InternetState> {
+  final Connectivity connectivity;
+  late StreamSubscription connectivityStreamSubscription;
+  InternetCubit({required this.connectivity}) : super(InternetLoading()) {
+    connectivityStreamSubscription =
+        connectivity.onConnectivityChanged.listen((event) {
+      if (event == ConnectivityResult.mobile) {
+        emit(const InternetConnected(connectionType: ConnectionType.Mobile));
+      } else if (event == ConnectivityResult.wifi) {
+        emit(const InternetConnected(connectionType: ConnectionType.Wifi));
+      } else if (event == ConnectivityResult.none) {
+        emit(InternetDisconnected());
+      }
+    });
+  }
+
+  void emitInternetConnected(ConnectionType connectionType) =>
+      emit(InternetConnected(connectionType: connectionType));
+
+  void emitInternetDisonnected() => emit(InternetDisconnected());
+
+  @override
+  Future<void> close() {
+    connectivityStreamSubscription.cancel();
+    return super.close();
+  }
+}
